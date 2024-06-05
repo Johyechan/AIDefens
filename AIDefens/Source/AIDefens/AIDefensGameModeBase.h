8e6ccc3898b92edeae43232ccaaaf87a26262bcb
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-06-05 12:52:11 +0900

    Start Project

---
+++ AIDefens/Source/AIDefens/AIDefensGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "AIDefensGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIDEFENS_API AAIDefensGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
