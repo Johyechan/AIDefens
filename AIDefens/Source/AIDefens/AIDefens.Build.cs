4742f73cecf3fe0b0147cb281e2c784363954f7a
Johyechan <98877897+Johyechan@users.noreply.github.com>
2024-06-12 11:26:12 +0900

    Add ProjectFile & GameMode Script & Player Script

--- AIDefens/Source/AIDefens/AIDefens.Build.cs
+++ AIDefens/Source/AIDefens/AIDefens.Build.cs
@@ -7,7 +7,9 @@ public class AIDefens : ModuleRules
 	public AIDefens(ReadOnlyTargetRules Target) : base(Target)
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
-	
+
+		PublicIncludePaths.AddRange(new string[] { "AIDefens" });
+
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
---
+++ AIDefens/Source/AIDefens/Game/ADGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ADGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class AIDEFENS_API AADGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
